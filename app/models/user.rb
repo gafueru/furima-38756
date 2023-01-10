f5bb05af35b22f90f1b906c70749abcef787fa7e
gafueru <gafuri1780@gmail.com>
2023-01-10 19:02:52 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :birthday, presence: true
+         with_options presence: true do
+          validates :nickname, :birthday, :password_confirmation
+          validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
+          validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+        end
+        validates :password, :password_confirmation,
+        format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+      end
 end
