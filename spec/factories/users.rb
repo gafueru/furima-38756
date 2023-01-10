4db5ad44a31ce0ad5b13cbff97f0cd9d920a1ce7
gafueru <gafuri1780@gmail.com>
2023-01-10 19:28:49 +0900

    gimeiの導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    transient do
+      person { Gimei.name }
+  end
+    birthday { Faker::Date.backward }
+    first_name { person.first.kanji }
+    last_name { person.last.kanji }
+    first_name_kana { person.first.katakana }
+    last_name_kana { person.last.katakana }
   end
 end
