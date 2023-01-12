043b83c268dfda788220fedcdcef8e26c00ec661
gafueru <gafuri1780@gmail.com>
2023-01-12 15:50:34 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
   validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
