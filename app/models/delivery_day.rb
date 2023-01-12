fc253058ef322fc7e0a34e8f6f65fae16d7c3412
gafueru <gafuri1780@gmail.com>
2023-01-12 19:30:36 +0900

    商品出品機能の実装

--- app/models/delivery_day.rb
+++ app/models/delivery_day.rb
@@ -1,5 +1,5 @@
-class Delivery_day < ActiveHash::Base
-  self.date = [
+class DeliveryDay < ActiveHash::Base
+  self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '1~2日で発送'},
     { id: 3, name: '2~3日で発送'},
@@ -8,5 +8,5 @@ class Delivery_day < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :item
-  
+
 end
