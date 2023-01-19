ea20a06dec5409b42d7d092cad73b1afefbe4dbe
gafueru <gafuri1780@gmail.com>
2023-01-19 12:43:54 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: %i[new create index show edit update destroy]
+  resources :items, only: %i[new create index show edit update destroy] do
+    resources :buys, only: :create
+  end
 end
