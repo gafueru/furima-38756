da01167a828a1aa3122d82dbfbe152d4b223f6c3
gafueru <gafuri1780@gmail.com>
2023-01-20 21:03:59 +0900

    内容修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const payjp = Payjp('pk_test_9272d61bb296bb39872d0036')
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -26,6 +25,7 @@ const pay = () => {
         cvcElement.clear();
         document.getElementById("charge-form").submit();
       }
+    });
   });
 };
 
