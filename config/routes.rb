f54d55ed9d0af7910c04411d06983c57c67bf4f6
gafueru <gafuri1780@gmail.com>
2023-01-23 17:39:06 +0900

    内容修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: %i[new create index show edit update destroy] do
-    resources :buys, only: %i[index new create]
+    resources :buys, only: %i[index create]
   end
 end
